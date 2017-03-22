64be0a2e3a11ea73eccc6a8f91c0b001e26e7591
Michael K. Hunter <hunter.michael.k@gmail.com>
2017-03-22 10:58:25 +0000

    Test now running successfully. Basic HTML rendered by the view.

--- michael/views.py
+++ michael/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse()
+    return HttpResponse("<!DOCTYPE html><html><title>Michael Hunter's Homepage</title></html>")
+
