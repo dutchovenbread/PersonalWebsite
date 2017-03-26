d88467b8064c02853e9b40cd8394c2ce7248e0c0
Michael Hunter <hunter.michael.k@gmail.com>
2017-03-25 22:34:54 -0400

    Added check of the HTTP status to test.

--- michael/tests.py
+++ michael/tests.py
@@ -16,6 +16,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+        self.assertEqual(response.status_code, 200)
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<!DOCTYPE html>'))
         self.assertIn("Michael Hunter's Homepage", html)
@@ -26,6 +27,5 @@ class HomePageTest(TestCase):
         response = home_page(request)
         html = response.content.decode('utf8')
         self.assertIn("<h1>Michael Hunter</h1>", html)
-
 
 
