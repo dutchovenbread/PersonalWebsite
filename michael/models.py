dd3e0166af984e17e0d26e82303fce1fc9e7ef0e
Michael Hunter <hunter.michael.k@gmail.com>
2017-03-27 06:46:38 -0400

    I have a 'Is this module globally installed' error.

--- michael/models.py
+++ michael/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    firstName = models.TextField(default='')
+    middleInitial = models.TextField(default='')
+    lastName = models.TextField(default='')
+
+class Education(models.Model):
+    person = models.ForeignKey(Person, default=None)
+    degree = models.TextField(default='')
+    major = models.TextField(default='')
+    school = models.TextField(default='')
+    yearCompleted = models.TextField(default='')
+
+    class Meta:
+        ordering = ('-yearCompleted',)
+
+class Job(models.Model):
+    person = models.ForeignKey(Person, default=None)
+    jobTitle = models.TextField(default='')
+    employer = models.TextField(default='')
+    startYear = models.TextField(default='')
+    endYear = models.TextField(default='')
+
+
+
