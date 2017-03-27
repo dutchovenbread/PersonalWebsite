838f2d18332c118f1a793fbb45b4665360d639da
Michael Hunter <hunter.michael.k@gmail.com>
2017-03-27 06:47:21 -0400

    I have a 'is this module globally installed' error.

---
+++ michael/tests/test_models.py
@@ -0,0 +1,12 @@
+from michael.models import Person, Education, Job
+
+from django.test import TestCase
+
+class JobModelTest(TestCase):
+    pass
+
+class EducationModelTest(TestCase):
+    pass
+
+class PersonModelTest(TestCase):
+    pass
